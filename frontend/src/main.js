ffbaec0ba3eb4125ce042f547132996c95f078e7
elva <elva@within.co.kr>
2018-08-13 16:21:51 +0900

    vuetify test initial commit

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,12 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
-import router from './router';
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import VueRouter from 'vue-router'
+import routes from './router'
 
-Vue.config.productionTip = false;
+Vue.use(VueRouter)
+Vue.use(Vuetify)
+const router = new VueRouter({routes})
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>',
-});
+}).$mount('#app')
---
+++ frontend/src/router.js
@@ -0,0 +1,15 @@
+import { Home, Calendar } from './Components/index'
+
+const routes = [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/calendar',
+    component: Calendar
+  }
+]
+
+
+export default routes
